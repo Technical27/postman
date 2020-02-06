e52652f303cf8f53b71e2deffbd73f64b5ae77c4
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2020-02-06 14:45:10 -0500

    debug command can now display a raw post

--- src/app/commands/debug.rs
+++ src/app/commands/debug.rs
@@ -1,11 +1,18 @@
 use serenity::client::Context;
-use serenity::framework::standard::{macros::command, CommandResult};
+use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::Message;
 
-use super::helpers::{get_version, send_text};
+use super::helpers::{get_version, parse_post, send_post, send_text};
+use super::reddit::get_reddit_api;
 
 #[command]
-pub fn debug(ctx: &mut Context, msg: &Message) -> CommandResult {
-    send_text(ctx, msg, &format!("version: {}", get_version()))?;
-    Ok(())
+pub fn debug(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    match args.single_quoted::<String>() {
+        Ok(arg) => {
+            let data = get_reddit_api(arg.as_str())?;
+            let post = parse_post(&data[0]["data"]["children"][0])?;
+            send_post(ctx, msg, &post)
+        }
+        Err(_) => send_text(ctx, msg, &format!("version: {}", get_version())),
+    }
 }
