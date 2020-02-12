c9f690acf99a0d487495359628a1b9e4d3552fc8
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2020-02-12 13:54:52 -0500

    refactor random to a recursive function

--- src/commands/random.rs
+++ src/commands/random.rs
@@ -2,36 +2,31 @@ use serenity::client::Context;
 use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::Message;
 
+use std::thread;
+use std::time::Duration;
+
 use super::helpers::*;
-
 use super::post::Post;
-
 use super::reddit::*;
 
-fn get_random(sub: &str, nsfw: bool) -> RedditResult<Post> {
-    let mut i = 0;
-
-    let post = loop {
-        if i == 5 {
-            return Err(RedditAPIError::new("cant find any image"));
+fn get_random(sub: &str, nsfw: bool, tries: u8) -> RedditResult<Post> {
+    thread::sleep(Duration::from_millis(100));
+    let res = get_reddit_api(&format!("https://reddit.com/r/{}/random.json", sub))?;
+    if tries > 4 {
+        return Err(RedditAPIError::new("can't find any post"));
+    }
+    if let Ok(post) = parse_post(&res[0]["data"]["children"][0]) {
+        if post.nsfw && !nsfw {
+            return get_random(sub, nsfw, tries + 1);
         }
-        let res = get_reddit_api(&format!("https://reddit.com/r/{}/random.json", sub))?;
-        if let Ok(post) = parse_post(&res[0]["data"]["children"][0]) {
-            if post.nsfw && !nsfw {
-                i += 1;
-                continue;
-            }
-            break post;
-        }
-        i += 1;
-    };
-
-    Ok(post)
+        return Ok(post);
+    }
+    return get_random(sub, nsfw, tries + 1);
 }
 
 #[command]
 pub fn random(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
-    match get_random(&parse_sub(args)?, check_nsfw(ctx, msg)?) {
+    match get_random(&parse_sub(args)?, check_nsfw(ctx, msg)?, 1) {
         Ok(post) => send_post(ctx, msg, &post),
         Err(err) => send_text(ctx, msg, &format!("`{}`", err)),
     }
